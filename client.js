8d849a0dc4f4392c3feb08e9ed279fc9594724ab
Adam Bergstrom <abx1138@gmail.com>
2022-11-27 11:48:49 -0600

    completed budget practice

--- client.js
+++ client.js
@@ -16,6 +16,33 @@ function addPurchase() {
 
     $('#purchaseNameIn').val( '' );
     $('#purchasePriceIn').val( '' );
+
+    calculateRemainingBudget();
+    displayPurchases();
+}
+
+function calculateRemainingBudget() {
+    console.log('in calculateRemainingBudget');
+    let totalPrices = 0;
+    for(let i = 0; i < purchases.length; i++) {
+        totalPrices += Number(purchases[i].price);
+    }
+    console.log('totalPrices:', totalPrices);
+
+    const remainingBudget = budget - totalPrices;
+
+    let el = $( '#remainingBudgetOut' );
+    el.empty();
+    el.append( remainingBudget );
+}
+
+function displayPurchases(){
+    console.log('in displayPurchases');
+    let el = $( '#purchasesOut');
+    el.empty();
+    for (purchase of purchases ){
+        el.append( '<li>' + purchase.name + ': $' + purchase.price + '</li>' );
+    }
 }
 
 function readyNow() {
@@ -24,4 +51,6 @@ function readyNow() {
     el.append( budget );
 
     $( '#addPurchaseButton' ).on( 'click', addPurchase );
+
+    calculateRemainingBudget();
 }
